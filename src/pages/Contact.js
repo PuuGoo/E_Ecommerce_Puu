a52324f4275be27f99069ce4557ff8661fada402
PuuGoo <ndphu1997@gmail.com>
2023-11-27 02:43:54 +0700

    Contact Page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,114 @@
 import React from "react";
+import Meta from "../components/Meta";
+import BreadCrumb from "../components/BreadCrumb";
+import {
+  FaEnvelope,
+  FaExclamationCircle,
+  FaHome,
+  FaPhone,
+} from "react-icons/fa";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <Meta title={"Contact Us"} />
+      <BreadCrumb title="Contact Us" />
+      <div className="contact-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d692.6930550365971!2d106.62623939921554!3d10.852296462274978!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752b161f50ae47%3A0x2328d1d1acc3b11a!2sFPT%20Polytechnic%20-%20T%C3%B2a%20F!5e0!3m2!1svi!2s!4v1701025550635!5m2!1svi!2s"
+                width="600"
+                height="450"
+                style={{ border: 0 }}
+                allowfullscreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+            <div className="col-12 mt-5">
+              <div className="contact-inner-wrapper d-flex justify-content-between">
+                <div>
+                  <h3 className="contact-title">Contact</h3>
+                  <form action="" className="d-flex flex-column gap-20">
+                    <div>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Name"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Email"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="tel"
+                        className="form-control"
+                        placeholder="Mobile Number"
+                      />
+                    </div>
+                    <div>
+                      <textarea
+                        name=""
+                        id=""
+                        className="w-100 form-control"
+                        cols="30"
+                        rows="4"
+                        placeholder="Comments"
+                      ></textarea>
+                    </div>
+                    <div>
+                      <button type="submit" className="button border-0">
+                        Submit
+                      </button>
+                    </div>
+                  </form>
+                </div>
+                <div>
+                  <h3 className="contact-title">Get in touch with us</h3>
+                  <div>
+                    <ul className="ps-0">
+                      <li className="mb-3 d-flex align-items-center gap-15">
+                        <FaHome className="fs-5" />
+                        <address className="mb-0">
+                          Hno : 277 Near Vill chopal, Sonipat, Haryana, PinCode:
+                          131103
+                        </address>
+                      </li>
+                      <li className="mb-3 d-flex align-items-center gap-15">
+                        <FaPhone className="fs-5" />
+                        <a href="tel:+84 984004258">
+                          +84 984004258
+                        </a>
+                      </li>
+                      <li className="mb-3 d-flex align-items-center gap-15">
+                        <FaEnvelope className="fs-5" />
+                        <a
+                          href="mailto: puugoo002@gmail.com"
+                        >
+                          puugoo002@gmail.com
+                        </a>
+                      </li>
+                      <li className="mb-3 d-flex align-items-center gap-15">
+                        <FaExclamationCircle className="fs-5" />
+                        <p className="mb-0">Monday - Friday 10 AM - 8 PM</p>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Contact;
